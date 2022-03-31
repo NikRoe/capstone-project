ae42000ca952ab1ef82f15a9dbbcd4d14d12c04b
Niklas Röttger <niklas.roettger@outlook.de>
2022-03-31 10:51:08 +0200

    add seriescard story

---
+++ components/SeriesCard/SeriesCard.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { SeriesCard } from "./SeriesCard";
+
+export default {
+  title: "Components/SeriesCard",
+  component: SeriesCard,
+};
+
+const exampleSeries = {
+  name: "The Man Who Saved Central City",
+  overview: "some long text",
+  poster_path: "/ohGz4HDYGTite1GmRhRuBMVAn03.jpg",
+  id: 222,
+};
+
+export const DefaultCard = () => (
+  <SeriesCard series={exampleSeries}></SeriesCard>
+);
