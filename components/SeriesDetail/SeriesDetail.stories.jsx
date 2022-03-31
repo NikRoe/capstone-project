4890c4a7c7b4000f3a70187921bb13f5ce4c026c
Niklas Röttger <niklas.roettger@outlook.de>
2022-03-31 11:09:06 +0200

    add seriesdetail story

---
+++ components/SeriesDetail/SeriesDetail.stories.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { SeriesDetail } from "./SeriesDetail";
+
+export default {
+  title: "Components/SeriesDetail",
+  component: SeriesDetail,
+};
+
+const exampleSeries = {
+  name: "The Man Who Saved Central City",
+  overview: "some long text",
+  poster_path: "/ohGz4HDYGTite1GmRhRuBMVAn03.jpg",
+  seasons: [{ name: "Episode 1", id: 1 }],
+};
+
+export const DefaultSeries = () => (
+  <SeriesDetail series={exampleSeries}></SeriesDetail>
+);
