e38fb38ea9ac9317aca98563859d9f60e2c893f4
Niklas Röttger <niklas.roettger@outlook.de>
2022-03-31 10:13:43 +0200

    add extrainfo story

---
+++ components/SeriesExtraInfo/SeriesExtraInfo.stories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { SeriesExtraInfo } from "./SeriesExtraInfo";
+
+export default {
+  title: "Components/SeriesExtraInfo",
+  component: SeriesExtraInfo,
+};
+
+const exampleSeries = {
+  last_air_date: "25-01-22",
+  number_of_seasons: 3,
+  number_of_episodes: 25,
+};
+
+export const DefaultEpisode = () => (
+  <SeriesExtraInfo series={exampleSeries}></SeriesExtraInfo>
+);
