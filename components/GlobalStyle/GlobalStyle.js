5cc5f8fc91ba2a07df9eed3e28ecf418e26910e5
Niklas Röttger <niklas.roettger@outlook.de>
2022-08-01 17:49:46 +0200

    restructure globalstyles

--- components/GlobalStyle/GlobalStyle.js
+++ components/GlobalStyle/GlobalStyle.js
@@ -3,6 +3,10 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
     box-sizing: border-box;
+    margin: 0;
+  }
+  
+  :root {
     --main-text-color: #eee;
     --button-text-color: black;
     --main-background-color: #525252F0;
@@ -10,13 +14,9 @@ export const GlobalStyle = createGlobalStyle`
     --main-callToAction-background-color: #00adb5;
     --highlighted-callToAction-background-color: #00d8e3;
   }
-
-  :root {
-    font-family: "Roboto", sans-serif;
-    margin: 0;
-  }
-
+  
   body {
+    font-family: "Roboto", sans-serif;
     display: flex;
     justify-content: center;
     text-align: center;
--- pages/api/getSeriesBySearch/[searchTerm].js
+++ pages/api/getSeriesBySearch/[searchTerm].js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default async function getSeriesBySearch(req, res) {
-  console.log(req.query);
   const URL = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.API_KEY}&language=en-US&page=1&query=${req.query.searchTerm}`;
   const response = await axios.get(URL);
   res.status(200).json({ data: response.data });
